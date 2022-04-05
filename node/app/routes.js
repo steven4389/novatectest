5ce96b14617eb1b56614657fd7c259b9177f052d
steven4389 <40493520+steven4389@users.noreply.github.com>
2022-04-05 17:22:53 -0500

    first commit

---
+++ node/app/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+// Importar controladores
+const ProductController = require('../app/controllers/ProductController');
+const UserController = require('../app/controllers/UserController');
+
+
+// Products
+ router.post('/', ProductController.create);
+ router.get('/getByName/:name', ProductController.showByKeyWord);
+ router.get('/', ProductController.sowAll);
+ router.get('/:id', ProductController.showById);
+ router.patch('/:id', ProductController.update);
+ router.delete('/:id', ProductController.delete);
+ router.post('/createUser', UserController.create);
+ router.get('/users/pendientes', UserController.showUsers);
+ router.get('/test/test', (req, res)=>{ 
+	res.json({"message": "aplicacion desplegada"});
+ });
+
+
+module.exports = router;
---
+++ node/app/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+var cors = require('cors')
+
+// Setting
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+// Para poder rellenar el req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+// Rutas
+app.use('/api',require('./routes'));
+
+// Arrancamos el servidor
+app.listen(PORT, function () {
+    console.log(`La app ha arrancado en http://localhost:${PORT}`);
+
+    sequelize.sync({ force: false }).then(() => {
+        console.log("Se ha establecido la conexión");
+    })
+});
